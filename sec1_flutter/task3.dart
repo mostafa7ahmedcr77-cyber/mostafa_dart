13ce4d6d21118f380f4256bc0f118526772302c1
mostafa_github <mostafa7ahemdcr77@gmail.com>
2026-08-25 16:26:59 +0300

    /task3/

---
+++ sec1_flutter/task3.dart
@@ -0,0 +1,11 @@
+void main() {
+  String studentName = "Ahmed";
+  String grade = "A";
+  String schoolName = "Future School";
+  bool isPresent = true;
+
+  print("Student Name: $studentName");
+  print("Grade: $grade");
+  print("School Name: $schoolName");
+  print("Present: $isPresent");
+}

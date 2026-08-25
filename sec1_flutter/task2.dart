55f8c795d17728cd055cefc9019015662c6817ab
mostafa_github <mostafa7ahemdcr77@gmail.com>
2026-08-25 16:15:34 +0300

    /task2/

---
+++ sec1_flutter/task2.dart
@@ -0,0 +1,9 @@
+void main() {
+  String productName = "Laptop";
+  double productPrice = 25000;
+  int productQuantity = 3;
+
+  print("Product: $productName");
+  print("Price: $productPrice");
+  print("Quantity: $productQuantity");
+}

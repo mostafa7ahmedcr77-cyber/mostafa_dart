51afeb3d8ce1330301b3679b3a51eb00f3cda619
mostafa_github <mostafa7ahemdcr77@gmail.com>
2026-08-25 16:32:35 +0300

    /task4/

---
+++ sec1_flutter/task4.dart
@@ -0,0 +1,11 @@
+void main() {
+  String favoriteMovie = "Interstellar";
+  String favoriteFood = "Pizza";
+  int favoriteNumber = 7;
+  String favoriteSport = "Football";
+
+  print("Favorite Movie: $favoriteMovie");
+  print("Favorite Food: $favoriteFood");
+  print("Favorite Number: $favoriteNumber");
+  print("Favorite Sport: $favoriteSport");
+}

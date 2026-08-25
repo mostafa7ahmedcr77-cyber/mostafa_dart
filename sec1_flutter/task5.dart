a7e814e03c91f5fa2ed7c17f31a67f9c5f8eb058
mostafa_github <mostafa7ahemdcr77@gmail.com>
2026-08-25 16:33:58 +0300

    /task5/

---
+++ sec1_flutter/task5.dart
@@ -0,0 +1,11 @@
+void main() {
+  String ownerName = "Ahmed";
+  int accountNumber = 123456;
+  double balance = 5000.75;
+  bool isActive = true;
+
+  print("Owner: $ownerName");
+  print("Account Number: $accountNumber");
+  print("Balance: $balance");
+  print("Active: $isActive");
+}
